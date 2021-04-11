b635137f2103571937b1506f491d15abb2d94b7d
Simono il magnifico <simoneromano@tutanota.de>
2021-04-11 16:45:47 +0200

    init bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("asdasdasd")
+import { Client } from 'discord.js';
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('token');
